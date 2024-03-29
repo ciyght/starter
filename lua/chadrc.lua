dc584e34bab069f9c61e8bda55f965948adfa4a6
Koen Reinders <koenf.reinders@gmail.com>
2024-03-29 11:35:25 +0100

    Change default theme
    Set default theme to cattpuccin and made the statusline separator "arrow" style

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,13 +5,23 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
+M.ui = {
+  theme = "catppuccin",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+
+  transparency = true,
+  statusline = {
+    -- default/vscode/vscode_colored/minimal
+    theme = "default",
+    -- default/round/block/arrow (separators work only for "default" statusline theme;
+    -- round and block will work for the minimal theme only)
+    separator_style = "arrow",
+    overriden_modules = nil,
+  },
 }
 
 return M
