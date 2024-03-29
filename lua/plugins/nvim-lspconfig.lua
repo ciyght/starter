10c30cf848c3441ec1a7c656270167e16a3d9861
Koen Reinders <koenf.reinders@gmail.com>
2024-03-29 11:52:11 +0100

    Remove null-ls dependency
    Null-ls was already replaced by nvim-lint in 87d230ed

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,14 +1,5 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = {
-    -- format & linting
-    {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "configs.null-ls"
-      end,
-    },
-  },
   config = function()
     require('nvchad.configs.lspconfig').defaults()
     require "configs.lspconfig"
