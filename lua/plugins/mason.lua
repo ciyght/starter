e6b24c2434eb0eeaf62d09d324a99c5ba5cca27c
Koen Reinders <koenf.reinders@gmail.com>
2024-03-29 11:20:24 +0100

    Add custom plugins

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,31 @@
+return{
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      -- lua stuff
+      "lua-language-server",
+      "stylua",
+
+      -- web dev stuff
+      "css-lsp",
+      "html-lsp",
+      "typescript-language-server",
+      "deno",
+      "prettier",
+
+      -- c/cpp stuff
+      "clangd",
+      "clang-format",
+
+      -- python
+      "black",
+      "flake8",
+      "mypy",
+      "pyright",
+
+      -- LaTeX
+      "texlab",
+      "ltex-ls"
+    }
+  }
+}
