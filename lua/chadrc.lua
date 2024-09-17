81c715b5100b691bf04a61c7ca2bc998c14e4621
Koen Reinders <koenf.reinders@gmail.com>
2024-09-17 14:50:18 +0200

    Move Mason packages to chadrc.options.mason.pkgs

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,49 +1,89 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+local options = {
 
----@type ChadrcConfig
-local M = {}
+    base46 = {
+      integrations = {
+        "blankline",
+        "cmp",
+        "defaults",
+        "devicons",
+        "git",
+        "lsp",
+        "mason",
+        "nvcheatsheet",
+        "nvdash",
+        "nvimtree",
+        "statusline",
+        "syntax",
+        "tbline",
+        "telescope",
+        "treesitter",
+        "rainbowdelimiters",
+        "todo",
+      },
+    }
 
-M.ui = {
-  theme = "gruvchad",
+    ui = {
+      theme = "gruvchad",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+        -- hl_override = {
+        -- 	Comment = { italic = true },
+        -- 	["@comment"] = { italic = true },
+        -- },
 
-  transparency = true,
-  statusline = {
-    -- default/vscode/vscode_colored/minimal
-    theme = "default",
-    -- default/round/block/arrow (separators work only for "default" statusline theme;
-    -- round and block will work for the minimal theme only)
-    separator_style = "arrow",
-    overriden_modules = nil,
-  },
+      transparency = true,
+      statusline = {
+        -- default/vscode/vscode_colored/minimal
+        theme = "default",
+        -- default/round/block/arrow (separators work only for "default" statusline theme;
+        -- round and block will work for the minimal theme only)
+        separator_style = "arrow",
+        overriden_modules = nil,
+      },
+    }
+
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+      enabled = true,
+      lazyload = true,
+      order = { "treeOffset", "buffers", "tabs", "btns" },
+      modules = nil,
+    },
+
+    mason = {
+      cmd = true,
+      pkgs = {
+      -- lua stuff
+      "lua-language-server",
+      "stylua",
+
+      -- web dev stuff
+      "css-lsp",
+      "html-lsp",
+      "typescript-language-server",
+      "deno",
+      "prettier",
+
+      -- c/cpp stuff
+      "clangd",
+      "clang-format",
+      "omnisharp@v1.39.8",
+
+      -- python
+      "black",
+      "flake8",
+      "mypy",
+      "pyright",
+
+      -- LaTeX
+      "texlab",
+      "ltex-ls",
+
+      -- rust
+      "codelldb",
+      "rust_analyzer",
+      }
+    },
 }
 
-M.base46 = {
-  integrations = {
-    "blankline",
-    "cmp",
-    "defaults",
-    "devicons",
-    "git",
-    "lsp",
-    "mason",
-    "nvcheatsheet",
-    "nvdash",
-    "nvimtree",
-    "statusline",
-    "syntax",
-    "tbline",
-    "telescope",
-    "treesitter",
-    "rainbowdelimiters",
-    "todo",
-  },
-}
-
-return M
+local status, chadrc = pcall(require, "chadrc")
+return vim.tbl_deep_extend("force", options, status and chadrc or {})
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,36 +1,3 @@
 return{
   "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      -- lua stuff
-      "lua-language-server",
-      "stylua",
-
-      -- web dev stuff
-      "css-lsp",
-      "html-lsp",
-      "typescript-language-server",
-      "deno",
-      "prettier",
-
-      -- c/cpp stuff
-      "clangd",
-      "clang-format",
-      "omnisharp@v1.39.8",
-
-      -- python
-      "black",
-      "flake8",
-      "mypy",
-      "pyright",
-
-      -- LaTeX
-      "texlab",
-      "ltex-ls",
-
-      -- rust
-      "codelldb",
-      "rust_analyzer",
-    }
-  }
 }
